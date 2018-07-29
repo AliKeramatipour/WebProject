99ac2f8da424548aa0e53347df4f9efefa1f3919
Lethal Pizza <alikeramatipour@yahoo.com>
2018-07-29 16:38:06 +0430

    API added

--- codes/app.py
+++ codes/app.py
@@ -1,17 +1,32 @@
-from flask import Flask, url_for, request, Response, jsonify, render_template
+from flask import Flask, url_for, request, Response, jsonify, render_template, redirect, session
+import requests
+import json
 
 app = Flask(__name__)
-"""localhost/?q=text"""
+app.secret_key = 'any random string'
 
 
 @app.route('/')
-def search_1():
-	return render_template('home.html')
+def home():
+	if 'username' in session :
+		return render_template('index.html', data=session['username'] )
+	else :
+		return render_template('home.html')
 
 @app.route('/login', methods=['POST'])
 def search_function():
-	print(">>> ", request.form)
-	return render_template('index.html', data=request.form['firstname'] + request.form['lastname'])
+	if request.form['username'] == 'admin' and request.form['password'] == '1234' :
+		session['username'] = request.form['username']
+		return render_template('main.html')
+	else :
+		return redirect("", code=302)
+
+@app.route('/search', methods=['GET'])
+def search_wikipedia():
+	search_string = request.args['query']
+	json_format = requests.get('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&exsentences=2&explaintext=&format=json&redirects=&formatversion=2&titles=' + search_string).content
+	array = json.loads(json_format)
+	return array['query']['pages'][0]['extract']
 
 if __name__ == '__main__':
 	app.run(port=5002)
